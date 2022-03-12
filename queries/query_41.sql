88d9cceb6309868a010384864808cdb0f58c0821
battery-lcf <79118064+battery-lcf@users.noreply.github.com>
2022-03-12 14:01:33 -0700

    Delete query_41.sql

--- queries/query_41.sql
+++
@@ -1,8 +0,0 @@
-/*
-Name: Filters: Cathode
-Data source: 1
-Created By: admin
-Last Update At: 2021-07-02T17:19:37.931Z
-*/
-
-select distinct cathode as a, count(*) from cell_metadata group by a order by a
