009584eb2f93dbfccf645c29fd773c569c3b2e3f
battery-lcf <79118064+battery-lcf@users.noreply.github.com>
2022-03-12 14:01:16 -0700

    Delete query_38.sql

--- queries/query_38.sql
+++
@@ -1,8 +0,0 @@
-/*
-Name: Filters: Anode
-Data source: 1
-Created By: admin
-Last Update At: 2021-07-02T17:20:01.513Z
-*/
-
-select distinct anode as a, count(*) from cell_metadata group by a order by a
