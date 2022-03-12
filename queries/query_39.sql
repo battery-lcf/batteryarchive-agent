fd8b4d57c4fb76791d777f3e10aeabef02a38e2b
battery-lcf <79118064+battery-lcf@users.noreply.github.com>
2022-03-12 14:01:22 -0700

    Delete query_39.sql

--- queries/query_39.sql
+++
@@ -1,8 +0,0 @@
-/*
-Name: Filters: C charge Rate
-Data source: 1
-Created By: admin
-Last Update At: 2021-07-02T17:19:55.071Z
-*/
-
-select distinct crate_c as a, count(*) from cycle_metadata group by a order by a
