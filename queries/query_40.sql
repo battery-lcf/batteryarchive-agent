1449df54103a1ad1bcae27c1382ec3fa9fcdfd45
battery-lcf <79118064+battery-lcf@users.noreply.github.com>
2022-03-12 14:01:28 -0700

    Delete query_40.sql

--- queries/query_40.sql
+++
@@ -1,8 +0,0 @@
-/*
-Name: Filters: C Discharge Rate
-Data source: 1
-Created By: admin
-Last Update At: 2021-07-02T17:19:46.265Z
-*/
-
-select distinct crate_d as a, count(*) from cycle_metadata group by a order by a
