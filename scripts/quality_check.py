b44cd2ea62ca033b3fc051e20c4c8c13a004e8c1
lnkassa <lnkassa@sandia.gov>
2024-10-08 15:14:31 -0600

    removing commented-out code

--- scripts/quality_check.py
+++ scripts/quality_check.py
@@ -7,18 +7,9 @@ import sys, getopt
 # Copyright 2024 National Technology & Engineering Solutions of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains certain rights in this software.
 
 # Prints charge and discharge capacity for selected cycles
-# Reads a CSV file with UCONN formatting as described in read_csv_rows()
+# Reads a CSV file with TONGJI-KIT formatting as described in read_csv_rows()
 
 def read_csv_rows(file_path):
-
-    # column names:
-    # Cycle_Index -> cycle_index
-    # Test_Time(s) -> test_time
-    # Current(A) -> i
-    # Voltage(V) -> v
-    # Date_Time -> date_time
-
-    #print("path: " + file_path)
 
     list_of_files = glob.glob(file_path + '*.csv*')
     df = pd.read_csv(list_of_files[0]) #todo: modify when adding multiple files
@@ -44,11 +35,6 @@ def read_csv_rows(file_path):
                     print('Charge capacity at cycle ' + str(c) + ': ' + str(df[capacity_c_col].loc[r-1]/3500))
             elif r == start_row+no_rows-1:
                 print('Discharge capacity at cycle ' + str(c) + ': ' + str(df[capacity_d_col].loc[r]/3500))
-#            if df[current_col].loc[r]<0:
-#                if df[current_col].loc[r-1]>=0:
-#                    print('Charge capacity at cycle ' + str(c) + ': ' + str(df[capacity_c_col].loc[r-1]))
-#                elif r == start_row+no_rows-1:
-#                    print('Discharge capacity at cycle ' + str(c) + ': ' + str(df[capacity_d_col].loc[r]))
 
 def main(argv):
     mode = 'env'
